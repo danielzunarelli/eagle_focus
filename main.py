26df97ad0f0576304f1c6b388db74e3a68558d87
Daniel Zunarelli <152896851+DHZuna@users.noreply.github.com>
2025-05-31 15:21:53 -0300

    "att 1.0"

---
+++ main.py
@@ -0,0 +1,13 @@
+from start_menu import start_menu
+
+print("\nWelcome to the Eagle Focus App\n")
+option = int(input(""
+"   1: Start\n"
+"   2: Focus Settings\n"
+"   3: Exit\n"
+"   \nSelect one option:"))
+
+
+
+if option == 1:
+    start_menu 
---
+++ start_menu.py
@@ -0,0 +1,7 @@
+def start_menu():
+    option_start = int(input(""
+"   1: Start\n"
+"   2: Focus Settings\n"
+"   3: Exit\n"
+"   \nSelect one option: "
+    ))
